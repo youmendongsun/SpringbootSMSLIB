138658c04bce52140c3ad6131a847ae369724d5d
lesaas <3011587388@qq.com>
2019-12-28 19:44:03 +0800

    simplify the code

--- src/main/java/com/lixinlei/SpringbootSMSLIB/service/SmsCatEnumInstance.java
+++ src/main/java/com/lixinlei/SpringbootSMSLIB/service/SmsCatEnumInstance.java
@@ -88,3 +88,41 @@ public enum SmsCatEnumInstance {
     }
 
 }
+
+class MessageWrapper {
+
+    public static SmsResult doWrap(OutboundMessage msg) {
+        SmsResult smsResult = new SmsResult();
+        smsResult.setStatus(Status.OK.getStatus());
+        smsResult.setDetail(msg.getMessageStatus().toString());
+        return smsResult;
+    }
+
+    public static SmsResult doWrapError(Exception e) {
+        SmsResult smsResult = new SmsResult();
+        smsResult.setStatus(Status.ERROR.getStatus());
+        smsResult.setDetail(e.getMessage());
+        return smsResult;
+    }
+
+    private enum Status {
+        OK("ok"),
+        ERROR("error"),
+        ;
+
+        private String status;
+
+        Status(String value) {
+            this.status = value;
+        }
+
+        public String getStatus() {
+            return status;
+        }
+
+        public void setStatus(String status) {
+            this.status = status;
+        }
+    }
+
+}
